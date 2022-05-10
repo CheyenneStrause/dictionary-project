5c35f997f3b0cae403cf57db5eabe9aefaab9a4b
CheyenneStrause <90160844+CheyenneStrause@users.noreply.github.com>
2022-05-10 09:32:20 -0700

    Created Dictionary.js component
    Created Dictionary.js component with a working form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Dictionary Project
+      <div className="container">
+        <header className="App-header text-center">Dictionary Project</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Cheyenne Strause</small>
+        </footer>
+      </div>
     </div>
   );
 }
